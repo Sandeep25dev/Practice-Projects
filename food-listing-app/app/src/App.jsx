f99277cd15052b1ad80903ba6e3714b6f8daaf22
Sandeep <sandeepkumar250903@gmail.com>
2024-08-24 16:41:16 +0530

    finished the top section of the UI

--- food-listing-app/app/src/App.jsx
+++ food-listing-app/app/src/App.jsx
@@ -1,7 +1,41 @@
 import styled from "styled-components";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <Container>
+      <TopSection>
+        <div className="logo">
+          <img src="/images/logo.svg" alt="foody zone logo" />
+        </div>
+        <div className="search">
+          <input type="text" placeholder="Search Food..." />
+        </div>
+      </TopSection>
+    </Container>
+  );
 }
 
 export default App;
+
+const Container = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+`;
+const TopSection = styled.section`
+  min-height: 140px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 16px;
+  .search {
+    input {
+      background-color: transparent;
+      border: 1px solid red;
+      border-radius: 5px;
+      color: white;
+      height: 40px;
+      font-size: 16px;
+      padding: 0 10px;
+    }
+  }
+`;
